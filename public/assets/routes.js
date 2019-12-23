b718d00bf5f381b4131573e9a2954fc1b4aefc29
Marissa Fortier <marissafortier@gmail.com>
2019-12-22 22:17:20 -0500

    added the delete notes function to be able to delete notes that have been made

--- public/assets/routes.js
+++ public/assets/routes.js
@@ -1,13 +1,13 @@
 const app = require("express");
-
+const index = require("../db/public/assets/index");
 
 // get notes from the notes page through the functions written on the index.js file
-app.get("/notes", function(req, res) {
+app.get("/notes", (req, res) => {
     index
       .getNotes()
       .then(notes => res.json(notes))
       .catch(err => res.status(500).json(err));
-  });
+  };
   
   
 
@@ -17,4 +17,15 @@ app.get("/notes", function(req, res) {
       .then((note) => res.json(note))
       .catch(err => res.status(500).json(err));
   });
+
+//   deleting the note if the id matches what is passed through the removeNote function, in this case, (req.params.id)
   
+  app.delete("/notes/:id", function(req, res) {
+    index
+      .removeNote(req.params.id)
+      .then(() => res.json({ notes: true }))
+      .catch(err => res.status(500).json(err));
+  });
+  
+module.exports = app;
+
